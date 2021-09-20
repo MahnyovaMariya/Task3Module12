a9770b62222bfc53eb9997ce60b85f6335b6493a
MahnyovaMariya <mahnyovamariya@gmail.com>
2021-09-20 23:20:13 +0300

    Initial Commit

---
+++ Skillbox_3Task12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox_3Task12
+//
+//  Created by Мария on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
